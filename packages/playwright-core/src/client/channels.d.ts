dea59db2de9b84f509d3eb6d21d2dd5fe3500c7b
Dmitry Gozman <dgozman@gmail.com>
2026-07-24 11:41:53 +0100

    chore(recorder): unify action coalescing and simplify action types (#41958)

--- packages/playwright-core/src/client/channels.d.ts
+++ packages/playwright-core/src/client/channels.d.ts
@@ -1358,7 +1358,7 @@ export type BrowserContextResponseEvent = {
   page?: PageChannel,
 };
 export type BrowserContextRecorderEventEvent = {
-  event: 'actionAdded' | 'actionUpdated' | 'signalAdded',
+  event: 'actionAdded' | 'signalAdded',
   data: any,
   page: PageChannel,
   code: string,
